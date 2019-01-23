6e1e0afe829b65ae3af7458b61e8cc848a2bb9cd
lgaudreau <44848504+lgaudreau@users.noreply.github.com>
2019-01-23 10:21:12 -0500

    fixes!

--- set megaraid email settings.ps1
+++ set megaraid email settings.ps1
@@ -1,40 +1,45 @@
-﻿#Configures the MegaRAID Storage Manager on specified servers to send email for Critical & fatal alerts.
+#Configures the MegaRAID Storage Manager on specified servers to send email for Critical & fatal alerts.
 
-$hosts = "host" #Get-SCVMHost | select ComputerName # - example.  Command can do array of computer names
+$hosts = "ServerName"  # - example.  Can accept array of computer names like 'Get-SCVMHost | select ComputerName'
 
 foreach ($comp in $hosts) {
+	
+    $comp = $($comp).ToUpper()
+	
+    $path = "\\$($comp)\c`$\Program Files (x86)\MegaRAID Storage Manager\MegaMonitor\config-current.xml"
+	
+    $sender = "$comp@myfrhi.com"
+    $servername = "torsmtp.myfairmont.com"
+    $recipient = "fhr.dcalert.dl@frhi.com"
 
-$comp = $($comp).ToUpper()
+    [string]$ipaddress = (Test-Connection $comp -Count 2).IPV4Address.IPAddressToString[0]
+	
 
-$path = "\\$($comp)\c`$\Program Files (x86)\MegaRAID Storage Manager\MegaMonitor\config-current.xml"
+    $MSMXML = (Select-Xml -Path $path -XPath /).Node
 
-$sender = "$comp@sender.com"
-$servername = "mailserver.sender.com" 
-$recipient = "recipient@sender.com"
+    #Set email
+    $MSMXML.'monitor-config'.actions.email.nic = $ipaddress
+    $MSMXML.'monitor-config'.actions.email.sender = $sender
+    $MSMXML.'monitor-config'.actions.email.'email-target' = $recipient
+    $MSMXML.'monitor-config'.actions.email.servername = $servername
+    $MSMXML.'monitor-config'.actions.email.'authentication-type' = "none"
 
-$MSMXML = ( Select-Xml -Path $path -XPath / ).Node
+    #If critical alerts have not been set to email, add email
+    $SetAlert = $MSMXML.'monitor-config'.global.severity.Item(1)
 
-#Set email
-$MSMXML.'monitor-config'.actions.email.sender = $sender
-$MSMXML.'monitor-config'.actions.email.'email-target' = $recipient
-$MSMXML.'monitor-config'.actions.email.servername = $servername
+    if (!($SetAlert.ChildNodes.ToString() -like "*do-email*")) {
+	
+        $attrib = $SetAlert.OwnerDocument.CreateElement("do-email")
+        $SetAlert.AppendChild($attrib)
+	
+    }
 
-#If critical alerts have not been set to email, add email
-$SetAlert = $MSMXML.'monitor-config'.global.severity.Item(1)
+    $MSMXML.Save($path) # Save changes
 
-if(!($SetAlert.ChildNodes.ToString() -like "*do-email*")) {
+    $MSMXML_Verify = (Select-Xml -Path $path -XPath /).Node #Read file to verify changes
 
-$attrib = $SetAlert.OwnerDocument.CreateElement("do-email")
-$SetAlert.AppendChild($attrib)
-
-}
-
-$MSMXML.Save($path) # Save changes
-
-$MSMXML_Verify = ( Select-Xml -Path $path -XPath / ).Node #Read file to verify changes
-
-$comp
-write-output "Sender: "$MSMXML_Verify.'monitor-config'.actions.email.sender""
-write-output "Recipient: "$MSMXML_Verify.'monitor-config'.actions.email.'email-target'""
+    $comp
+    write-output "Sender: "$MSMXML_Verify.'monitor-config'.actions.email.sender""
+    write-output "Recipient: "$MSMXML_Verify.'monitor-config'.actions.email.'email-target'""
 
 }
